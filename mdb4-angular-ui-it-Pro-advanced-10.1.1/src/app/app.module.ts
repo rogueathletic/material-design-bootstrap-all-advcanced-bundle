b8ce5a5543dad220d7891f7102c6c4fbe895144e
jason schutz <jasonschutz@schutzmtkinsmfg.lan>
2021-03-30 00:07:56 -0700

    added angular pro ui kit

---
+++ mdb4-angular-ui-it-Pro-advanced-10.1.1/src/app/app.module.ts
@@ -0,0 +1,46 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+
+import { MDBSpinningPreloader, MDBBootstrapModulesPro, ToastModule } from 'ng-uikit-pro-standard';
+import { MdbCalendarModule } from 'mdb-calendar';
+import { MdbColorPickerModule } from 'mdb-color-picker';
+import { MdbDraggableModule } from 'mdb-draggable';
+import { MdbFileUploadModule } from 'mdb-file-upload';
+import { MdbSortableModule } from 'mdb-sortable';
+import { MdbTableEditorModule } from 'mdb-table-editor';
+import { MdbWysiwygModule } from 'mdb-wysiwyg';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ToastModule.forRoot(),
+    MDBBootstrapModulesPro.forRoot(),
+    MdbCalendarModule,
+    MdbColorPickerModule,
+    MdbDraggableModule,
+    MdbFileUploadModule,
+    MdbSortableModule,
+    MdbTableEditorModule,
+    MdbWysiwygModule,
+    AgmCoreModule.forRoot({
+      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en#key
+      apiKey: 'Your_api_key'
+    })
+  ],
+  providers: [MDBSpinningPreloader],
+  bootstrap: [AppComponent],
+  schemas:      [ NO_ERRORS_SCHEMA ]
+})
+export class AppModule { }
